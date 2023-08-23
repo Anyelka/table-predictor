e5b00f118352f0e411e21b8f0cb888004c739e9e
marton.takacs <marton.takacs@tesco.com>
2023-08-23 15:06:28 +0200

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAscQjTt9Hx0n8J6I8_tljMzCXjfWLRUP4",
+  authDomain: "table-predictor.firebaseapp.com",
+  projectId: "table-predictor",
+  storageBucket: "table-predictor.appspot.com",
+  messagingSenderId: "913305455937",
+  appId: "1:913305455937:web:cf26d07a28367f7711e5c0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
